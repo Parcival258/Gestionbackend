81d8682f788b64ab8bbbd27c6767696227b5f459
IngAlim2023 <jorgeeporrassandoval@gmail.com>
2025-06-11 22:42:13 -0500

    Arreglos

--- app/controller/Http/UsuariosController.ts
+++ app/controller/Http/UsuariosController.ts
@@ -6,18 +6,17 @@ class UsuariosController {
   async register({ request, response }: { request: any; response: any }) {
     const { email, password } = request.body()
     const newpass = await hash.make(password)
-    const rest = await PgDatabase.query(`INSTER INTO usuarios (email,password) VALUES($1,$2)`, [
+    const rest = await PgDatabase.query(`INSERT INTO usuarios (email,password) VALUES($1,$2)`, [
       email,
       newpass,
     ])
-    console.log(rest)
-    return response.json({ ms: `añadido` })
+    return response.json({ ms: `agregado` })
   }
 
   //login
   async login({ request, response }: { request: any; response: any }) {
     const { email, password } = request.body()
-    const rest = await PgDatabase.query(`SELECT * FROM usuario WHERE email =$1`, [email])
+    const rest = await PgDatabase.query(`SELECT * FROM usuarios WHERE email =$1`, [email])
     if (rest.rows.length === 0) {
       return response.json({ ms: `usuario no encontrado` })
     }
--- start/routes.ts
+++ start/routes.ts
@@ -1,2 +1,3 @@
 import './routes/EquiposRoutes.js'
 import './routes/PresidenteRoutes.js'
+import './routes/UsuariosRoutes.js'
